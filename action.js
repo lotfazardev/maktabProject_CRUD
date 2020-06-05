a3832c72072f075ef9d0eb1e2dbff757df5d9695
Mohammad H. Lotfazar <lotfazardev@users.noreply.github.com>
2020-06-06 03:50:21 +0430

    add deleteAll contact feature

--- action.js
+++ action.js
@@ -1,5 +1,5 @@
 $(document).foundation();
-const userData = [] ;
+let userData = [];
 // function writeLocalStorage(data){
 //     localStorage.setItem("userData", (localStorage.getItem("userData") ? localStorage.getItem("userData") + "@+new" : "") + data);
 // }
@@ -13,13 +13,13 @@ const userData = [] ;
 // }
 // readLocalStorage()
 // renderTable()
-function renderTable(){
-    $('#tabel-data').html("")
-    for(let i = 0 ; i < userData.length ; i++){
-        $('#tabel-data').append(
-            `
+function renderTable() {
+  $('#tabel-data').html("")
+  for (let i = 0; i < userData.length; i++) {
+    $('#tabel-data').append(
+      `
             <tr>
-              <td>${i+1}</td>
+              <td>${i + 1}</td>
               <td>${userData[i].name}</td>
               <td>${userData[i].familyName}</td>
               <td>${userData[i].phoneNumber}</td>
@@ -39,20 +39,51 @@ function renderTable(){
               </td>
             </tr>
             `
-        )
-    }
+    )
+  }
 }
 $(document).ready(function () {
-    $('#add-contact-form').submit(function () {
-        const tmp = {}
-        for(let i = 0 ; i < $('.add-contact-inputs').length ; i++){
-            let input = $('.add-contact-inputs')[i]
-            tmp[input.name] = input.value
-            input.value = ""
+  $('#add-contact-form').submit(function () {
+    const tmp = {}
+    for (let i = 0; i < $('.add-contact-inputs').length; i++) {
+      let input = $('.add-contact-inputs')[i]
+      tmp[input.name] = input.value
+      input.value = ""
+    }
+    userData.push(tmp)
+    // writeLocalStorage(JSON.stringify(tmp))
+    renderTable()
+    return false
+  })
+
+  $('#delete-all-btn').click(function(){
+    if(!userData.length){
+      Swal.fire(
+        'موردی برای حذف وجود ندارد',
+        '',
+        'warning'
+      )
+    }else{
+      Swal.fire({
+        title: 'آیا از پاک کردن تمامی موارد اطمینان دارید ؟',
+        text: "توجه تمامی اطلاعات پاک شده غیر قابل بازگشت می باشد !",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'نه پاکشون نکن !',
+        confirmButtonText: 'آره همشونو پاک کن !'
+      }).then((result) => {
+        if (result.value) {
+          userData = []
+          renderTable()
+          Swal.fire(
+            'پاک شد!',
+            'تمامی مخاطبین با موفقیت پاک شد',
+            'success'
+          )
         }
-        userData.push(tmp)
-        // writeLocalStorage(JSON.stringify(tmp))
-        renderTable()
-        return false
-    })
+      })
+    }
+  })
 });
