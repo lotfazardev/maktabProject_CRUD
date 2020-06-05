fca9d415cb296a718a7f51b417dfdd0be6f3f44d
Mohammad H. Lotfazar <lotfazardev@users.noreply.github.com>
2020-06-06 01:35:59 +0430

    add add contact feature

--- action.js
+++ action.js
@@ -1,4 +1,44 @@
 $(document).foundation();
-$(document).ready(function(){
-    //app here
+const userData = [] ;
+function renderTable(){
+    $('#tabel-data').html("")
+    for(let i = 0 ; i < userData.length ; i++){
+        $('#tabel-data').append(
+            `
+            <tr>
+              <td>${i+1}</td>
+              <td>${userData[i].name}</td>
+              <td>${userData[i].familyName}</td>
+              <td>${userData[i].phoneNumber}</td>
+              <td>${userData[i].telNumber}</td>
+              <td>${userData[i].address}</td>
+              <td>${userData[i].date}</td>
+              <td>${userData[i].details}</td>
+              <td>
+              <div>
+                  <button type="submit" class="button warning UD-buttons update-btn">
+                    <span class="icon-pen"></span>
+                  </button>
+                  <button type="submit" class="button alert UD-buttons delete-btn">
+                    <span class="icon-bin"></span>
+                  </button>
+                </div>
+              </td>
+            </tr>
+            `
+        )
+    }
+}
+$(document).ready(function () {
+    $('#add-contact-form').submit(function () {
+        const tmp = {}
+        for(let i = 0 ; i < $('.add-contact-inputs').length ; i++){
+            let input = $('.add-contact-inputs')[i]
+            tmp[input.name] = input.value
+            input.value = ""
+        }
+        userData.push(tmp)
+        renderTable()
+        return false
+    })
 });
